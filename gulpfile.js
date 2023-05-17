d5c99d5d283057e4314a559428a6411c96576670
Ksenia Lavrukhina <burkamera@gmail.com>
2023-05-18 01:25:38 +0300

    Исправляет замечания

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ const copy = (done) => {
   gulp.src([
     'source/fonts/*.{woff2,woff}',
     'source/*.ico',
-    'source/manifest.json'
+    'source/manifest.webmanifest'
   ], {base:'source'
   })
     .pipe(gulp.dest('build'))
